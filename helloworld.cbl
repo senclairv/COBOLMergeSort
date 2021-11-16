77f874450d1f3d4dd447217482eb1b18882d2bda
senclairv <43869557+senclairv@users.noreply.github.com>
2021-11-15 18:38:26 -0600

    Add files via upload

---
+++ helloworld.cbl
@@ -0,0 +1,13 @@
+      ******************************************************************
+      * Author: Vincent SenClair
+      * Date: 4-13-18
+      * Purpose: Hello world program
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. HELLO-WORLD.
+       DATA DIVISION.
+       PROCEDURE DIVISION.
+       MAIN-PROCEDURE.
+            DISPLAY "HELLO WORLD".
+       END PROGRAM HELLO-WORLD.
